1413ab16bd016341b47bfc43387896737d736272
sangminkim <apfhdznzl@gmail.com>
2020-10-14 01:29:28 +0900

    compile 1.8 version and use activity-ktx for viewModels

--- livedata/app/src/main/java/com/sangmee/livedata/MainActivity.kt
+++ livedata/app/src/main/java/com/sangmee/livedata/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.sangmee.livedata
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.sangmee.livedata.databinding.ActivityMainBinding
 import com.sangmee.livedata.model.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var vm: MainViewModel
+    private val vm by viewModels<MainViewModel>()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
-
-        vm = ViewModelProvider(this, object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return MainViewModel() as T
-            }
-        }).get(MainViewModel::class.java)
         binding.vm = vm
 
     }
