682a4ffd46cb474f728ffd861f10cb26e872a3c9
sangminkim <apfhdznzl@gmail.com>
2020-10-29 16:27:06 +0900

    Set mainactivity and Update activitymain.xml for DataBinding

--- livedata/app/src/main/java/com/sangmee/livedata/MainActivity.kt
+++ livedata/app/src/main/java/com/sangmee/livedata/MainActivity.kt
@@ -2,12 +2,28 @@ package com.sangmee.livedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sangmee.livedata.databinding.ActivityMainBinding
 import com.sangmee.livedata.model.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var vm: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        vm = ViewModelProvider(this, object: ViewModelProvider.Factory{
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return MainViewModel() as T
+            }
+        }).get(MainViewModel::class.java)
+        binding.vm = vm
     }
 }
